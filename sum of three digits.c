d0a034e799dd30967440ca8cac82cfdaeb59f289
sushant86 <sushantaryal86@gmail.com>
2016-11-22 23:31:20 -0500

    Add files via upload

---
+++ sum of three digits.c
@@ -0,0 +1,25 @@
+\*
+student name:sushant aryal
+subject:programming fundamental
+roll no:17
+lab sheet:05
+date:2016/10/22
+*\
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float num1,num2,num3,sum;
+printf("enter first number\n");
+scanf("%f",num1);
+printf("enter second number\n");
+scanf("%f",num2);
+printf("enter third number\n");
+scanf("%f",num3);
+sum=num1+num2+num3;
+printf("the sum is %f",sum);
+return 0;
+}
+
---
+++ temperature.c
@@ -0,0 +1,23 @@
+\*
+student name:sushant aryal
+subject:programming fundamental
+roll no:17
+lab sheet:05
+date:2016/10/22
+*\
+
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float f,c;
+printf("enter fahrenhit\n");
+scanf("%f",f);
+centigrade=5/9*(f-32);
+printf("the centigrade is %f",centigrade);
+return 0;
+}
+
+
