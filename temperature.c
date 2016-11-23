3c8ece387fa421c4070d2141eb043f06c05df4fa
sushant86 <sushantaryal86@gmail.com>
2016-11-22 23:49:46 -0500

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -9,13 +9,13 @@ date:2016/10/22
 
 
 #include<stdio.h>
-#include<math.h>
+
 int main()
 {
-float f,c;
+float f,centigrade;
 printf("enter fahrenhit\n");
-scanf("%f",f);
-centigrade=5/9*(f-32);
+scanf("%f",&f);
+centigrade=0.56*(f-32);
 printf("the centigrade is %f",centigrade);
 return 0;
 }
