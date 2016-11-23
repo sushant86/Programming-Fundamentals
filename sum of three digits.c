20cd5d92f972cb09acbeed17421b1c39b6662af5
sushant86 <sushantaryal86@gmail.com>
2016-11-22 23:48:13 -0500

    Update sum of three digits.c

--- sum of three digits.c
+++ sum of three digits.c
@@ -1,3 +1,5 @@
+
+
 \*
 student name:sushant aryal
 subject:programming fundamental
@@ -8,18 +10,20 @@ date:2016/10/22
 
 
 #include<stdio.h>
-#include<math.h>
 int main()
 {
 float num1,num2,num3,sum;
+
 printf("enter first number\n");
-scanf("%f",num1);
+scanf("%f",&num1);
+
 printf("enter second number\n");
-scanf("%f",num2);
+scanf("%f",&num2);
+
 printf("enter third number\n");
-scanf("%f",num3);
+scanf("%f",&num3);
+
 sum=num1+num2+num3;
 printf("the sum is %f",sum);
 return 0;
 }
-
