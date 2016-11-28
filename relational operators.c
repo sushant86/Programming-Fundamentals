7de3768852ca696ddb6831b518108e8807a3b1c8
sushant86 <sushantaryal86@gmail.com>
2016-11-28 10:56:05 +0545

    Add files via upload

---
+++ relational operators.c
@@ -0,0 +1,34 @@
+/*
+Name:Sushant Aryal
+Subject:Programming Findamental
+Program:Write a program to use the relational operators
+Lab No:7
+Date:28th Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int x,y;
+printf("Enter the first number\n");
+scanf("%d",&x);
+printf("Enter the second number\n");
+scanf("%d",&y);
+x==y;
+x>y;
+x<y;
+x!=y;
+x>=y;
+x<=y;
+printf("The use of == operator is:%d\n",x==y);
+printf("The use of > operator is:%d\n",x>y);
+printf("The use of < operator is:%d\n",x<y);
+printf("The use of != operator is:%d\n",x!=y);
+printf("The use of >= operator is:%d\n",x>=y);
+printf("The use of <= operator is:%d\n",x<=y);
+getch();
+return 0;
+
+
+
+
+}
