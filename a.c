a44312032002ef29d252cb4e0c0ed623b0237688
sushant86 <sushantaryal86@gmail.com>
2017-01-16 10:56:47 +0545

    Add files via upload

---
+++ a.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int i,j,n;
+printf("enter any rows\n");
+scanf("%d",&n);
+for(i=1;i<=2;i++)
+{
+	for(j=1;j<=2;j++)
+{
+printf("1");
+}
+printf("\n");
+}
+return 0;
+}
---
+++ d.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int i,j,n;
+printf("enter any rows\n");
+scanf("%d",&n);
+for(i=1;i<=2;i++)
+{
+	for(j=1;j<=3;j++)
+{
+printf("%d",j);
+}
+printf("\n");
+}
+return 0;
+}
