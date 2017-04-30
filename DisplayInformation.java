a291a59438f04cee555f0fb286ae4075c30dda28
sushant86 <sushantaryal86@gmail.com>
2017-04-30 10:35:00 +0545

    Add files via upload

---
+++ DisplayInformation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+	class DisplayInformation{
+		public static void main(String args []){
+		String name, address, age;
+		Scanner inp=new Scanner(System.in);
+		System.out.println("Enter your name");
+		name=inp.next();
+		System.out.println("Enter your address");
+		address=inp.next();
+		System.out.println("Enter your age");
+		age=inp.next();
+		System.out.println("your name is "+name);
+		System.out.println("your address is "+address);
+		System.out.println("your age is "+age);
+
+	}
+}
+
+
